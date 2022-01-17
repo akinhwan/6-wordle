7d813e19e17f485c7bf98be5fcfada8ab63eb002
Andrew Kim <akinhwan@gmail.com>
2022-01-17 14:00:41 -0500

    add grid

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">6 letter Wordle</header> */}
+      <div className="Grid">
+        {[...Array(36)].map((_, i) => (
+          <div className="Cell">{i}</div>
+        ))}
+      </div>
     </div>
   );
 }
